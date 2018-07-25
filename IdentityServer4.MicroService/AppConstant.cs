f0dd356d833fc627293a13034fcff7963ccc6890
王振 <361790599@qq.com>
2018-07-25 17:06:24 +0800

    updated

--- IdentityServer4.MicroService/AppConstant.cs
+++ IdentityServer4.MicroService/AppConstant.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace IdentityServer4.MicroService
+﻿namespace IdentityServer4.MicroService
 {
     public class AppConstant
     {
